139a6ab0316fc54c8c0c0db51c9f9b7ae7c92632
sahith-55 <132653614+sahith-55@users.noreply.github.com>
2023-07-28 11:28:41 +0530

    Update Preethi_Tamilarasan.sql

--- Preethi_Tamilarasan.sql
+++ Preethi_Tamilarasan.sql
@@ -1,26 +1,35 @@
 --1.Write a query to display how many products were sold in February 2019.
-select count(*) as products_sold
+select count(Productname) as products_sold
 from ecommerce
-where purchase_date between '01-02-2019' and '29-02-2019';
+where purchase_date between '2019-02-01' and '2019-02-29';
 
 --2.Write a sql query to total sales amount in each year?
-select purchase_date as year,sum(price*quantity) as totalsale
-from ECOMMERCE
-group by purchase_date;
+SELECT (EXTRACT(YEAR FROM
+TO_DATE("PURCHASE_DATE",'YYYY-MM-DD'))) AS YEAR ,SUM(price*quantity) as totalsale
+FROM ecommerce
+GROUP BY (EXTRACT(YEAR FROM
+TO_DATE("PURCHASE_DATE",'YYYY-MM-DD')));
 
 --3.Write a sql query to what was the total sales amount of each product month_wise basis in the year 2019?
-select productname, sum(price*quantity) as totalsale
-from ECOMMERCE
-where purchase_date between '01-01-2019' and '30-12-2019'
-group by productname
-order by productname;
+SELECT productname,
+(EXTRACT(MONTH FROM TO_DATE("PURCHASE_DATE",'YYYY-MM-DD'))) AS month ,
+SUM(price*quantity) AS sales
+FROM ecommerce
+WHERE (EXTRACT(YEAR FROM
+TO_DATE("PURCHASE_DATE",'YYYY-MM-DD'))) = 2019
+GROUP BY productname,(EXTRACT(MONTH
+FROM TO_DATE("PURCHASE_DATE",'YYYY-MM-DD')))
+ORDER BY productname, (EXTRACT(MONTH
+FROM TO_DATE("PURCHASE_DATE",'YYYY-MM-DD')));
 
 --4.Write a sql query to count the customers from each country?
 select country,count(customerno) as No_of_cust
 from ECOMMERCE
 group by country; 
 
---Write a sql query to list all the unique productnames sold from each year?
-select distinct(PURCHASE_DATE),Productname
-from ECOMMERCE
-order by purchase_date;
+--5.Write a sql query to list all the unique productnames sold from each year?
+SELECT DISTINCT (EXTRACT(YEAR FROM
+TO_DATE("PURCHASE_DATE",'YYYY-MM-DD'))) AS Year, productname
+FROM ecommerce
+ORDER BY
+(EXTRACT(YEAR FROM TO_DATE("PURCHASE_DATE",'YYYY-MM-DD')));
