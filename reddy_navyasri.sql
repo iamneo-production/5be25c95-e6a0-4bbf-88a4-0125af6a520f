525e86e8e382713872c54f58a3e9f9e0e113bde2
ReddyNavyaSri <reddynavyasri89@gmail.com>
2023-07-29 10:56:58 +0000

    commit

--- reddy_navyasri.sql
+++ reddy_navyasri.sql
@@ -5,14 +5,14 @@ WHERE (EXTRACT(MONTH FROM
 TO_DATE("PURCHASE_DATE",'YYYY-MM-DD')))  = 2 and (EXTRACT(YEAR FROM
 TO_DATE("PURCHASE_DATE",'YYYY-MM-DD')))  = 2019;
 
--- 2 Write a SQL query to Total sales amount in each year.
+-- 2 Write a SQL query to Totalsales amount in each year.
 SELECT (EXTRACT(YEAR FROM
 TO_DATE("PURCHASE_DATE",'YYYY-MM-DD'))) AS YEAR ,SUM(price*quantity) as totalsale
 FROM ecommerce
 GROUP BY (EXTRACT(YEAR FROM
 TO_DATE("PURCHASE_DATE",'YYYY-MM-DD')));
 
--- 3 Write a SQL query to what was the total sales amount of each product month-wise basis in year 2019.
+-- 3 Write a SQL query to what was the total sales amount of each product month-wise basis in the year 2019.
 SELECT  productname,
 (EXTRACT(MONTH FROM TO_DATE("PURCHASE_DATE",'YYYY-MM-DD'))) AS month ,
 SUM(price*quantity) AS sales
